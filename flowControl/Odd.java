a6b8547532ca073c93b94b671150ec8d1d2140f0
Kamya Yadav | NorthernArc <kamya.yadav@NorthernArc.com>
2026-06-07 21:34:55 +0530

    Initial commit

---
+++ flowControl/Odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number:");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even number");
+        } else {
+            System.out.println(num + " is Odd number");
+        }
+
+        sc.close();
+    }
+}

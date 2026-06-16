bd1086a348ca05f8cf36fd569113d2c065eab42c
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-06-16 22:07:10 +0530

    Add files via upload

---
+++ Module 3/Day 1/multithreading/multithreading2/Main.java
@@ -0,0 +1,27 @@
+package Multithreading2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread("Sachin", 100);
+        MyThread t2 = new MyThread("Sourav", 400);
+        MyThread t3 = new MyThread("Rahul", 800);
+
+
+        t3.setDaemon(true);
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
+        System.out.println("Main threads finished");
+    }
+}

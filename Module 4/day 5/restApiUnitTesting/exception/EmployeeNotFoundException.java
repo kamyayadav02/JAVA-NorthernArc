5db0558c7fc54d417066c94e0f86371eff86a8e9
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-07-01 22:49:53 +0530

    Add files via upload

---
+++ Module 4/day 5/restApiUnitTesting/exception/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package org.northernarc.restapiforunittesting.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+
+}

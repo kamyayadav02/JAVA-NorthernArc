806ab6f4adfe66406497639d2fc0624e08f1b088
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-07-01 22:28:12 +0530

    Add files via upload

---
+++ Module 4/day 3/springSecurity/config/SpringSecurityConfig.java
@@ -0,0 +1,58 @@
+package org.northernarc.securitydemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SpringSecurityConfig {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+        @Bean
+        public UserDetailsService userDetailsService(){
+            System.err.println("User Details Service");
+            System.err.println("Encrypted password: " + passwordEncoder.encode("wowjava"));
+            UserDetails user1= User.builder()
+                    .username("user")
+                    .password(passwordEncoder.encode("hello"))
+                    .roles("USER")
+                    .build();
+            UserDetails admin=User.builder()
+                    .username("admin")
+                    .password(passwordEncoder.encode("hello"))
+                    .roles("ADMIN","USER")
+                    .build();
+            UserDetails loanofficer=User.builder()
+                    .username("loanofficer")
+                    .password(passwordEncoder.encode("hello"))
+                    .roles("LOAN_OFFICER","USER")
+                    .build();
+
+            return new InMemoryUserDetailsManager(user1,admin,loanofficer);
+        }
+
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            return http.authorizeHttpRequests(auth->auth
+                            .requestMatchers("/hello").permitAll()
+                            .anyRequest().authenticated())
+//                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults())
+                    .build();
+        }
+    }
+
+

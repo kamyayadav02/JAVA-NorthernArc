0b46b8c1d93dfa2182329eeba7b95efe835f79b5
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-07-01 21:35:17 +0530

    Add files via upload

---
+++ Weekly Assessment/weekly assessment-4/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package org.northernarc.assessment4.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.northernarc.assessment4.model.Customer;
+import org.northernarc.assessment4.repository.CustomerRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Customer not found with email: " + email));
+
+        return new User(
+                customer.getEmail(),
+                customer.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}

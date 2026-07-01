378ec34de7b71b58667475b9fe189a438cbe87a5
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-07-01 22:11:58 +0530

    Add files via upload

---
+++ Module 4/day 2/springSecurity/jpaUser/model/JPAUser.java
@@ -0,0 +1,12 @@
+package org.northernarc.securitydemo.model;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+public class JPAUser {
+    @Id
+    @
+
+}

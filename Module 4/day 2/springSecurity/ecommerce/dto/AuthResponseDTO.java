bb2d8fff645dd81365b03a72d42332e963d71ce7
kamyayadav02 <141482581+kamyayadav02@users.noreply.github.com>
2026-07-01 22:20:42 +0530

    Add files via upload

---
+++ Module 4/day 2/springSecurity/ecommerce/dto/AuthResponseDTO.java
@@ -0,0 +1,11 @@
+package org.northernarc.ecommerce.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+}
+
